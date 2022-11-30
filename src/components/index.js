646a32d51798f04aeb5f939eaa0af106623ad785
Uwaoma <uwaomaforex@gmail.com>
2022-11-30 15:46:58 +0000

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import NavBar from "./NavBar";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export{
+    Billing,
+    Business,
+    CardDeal,
+    Clients,
+    CTA,
+    Footer,
+    Hero,
+    NavBar,
+    Stats,
+    Testimonials,
+};
